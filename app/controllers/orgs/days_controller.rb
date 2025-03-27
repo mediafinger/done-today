15fcdf1d0feea6b02c7f613300d68fd11382b849
Andreas Finger <andy@mediafinger.com>
2025-03-27 23:25:26 +0100

    Make navigation to any date super flexible

--- app/controllers/orgs/days_controller.rb
+++ app/controllers/orgs/days_controller.rb
@@ -1,6 +1,15 @@
 module Orgs
   class DaysController < ApplicationController
     def index
+      if params[:date]
+        @day =
+          current_project.days.find_by(date: params[:date]) ||
+            current_project.days.build(org: current_org, date: params[:date])
+
+        render :show
+      else
+        @days = current_project.days
+      end
     end
 
     def show
--- app/controllers/orgs/entries_controller.rb
+++ app/controllers/orgs/entries_controller.rb
@@ -1,15 +1,24 @@
 module Orgs
   class EntriesController < ApplicationController
+    # when no day for the selected date exists yet, we initialize a new day,
+    #   so the user can start typing a log immediately
+    #
     def index
-      @day = current_project.days.find_by(date: params[:date])
+      @day =
+        current_project.days.find_by(date: params[:date]) ||
+          current_project.days.build(org: current_org, date: params[:date])
       @entries = @day.entries.where(member: current_member).order(status: :desc, created_at: :asc)
     end
 
+    # create_params[:date] is coming from a hidden form field and contains either
+    #   the day's date or the date given in the URL.
+    #   Therefore we need to find or create a day for the given date.
+    #
     def create
       entry = Entry.new(
         org: current_org,
         member: current_member,
-        day_id: create_params[:day_id], # hidden field in form
+        day: current_project.days.find_or_create_by!(org: current_org, date: create_params[:date]),
         log: create_params[:log],
         status: create_params[:status] || "todo", # TODO
       )
@@ -33,7 +42,7 @@ module Orgs
     private
 
     def create_params
-      params.require(:entry).permit(:day_id, :log, :status)
+      params.require(:entry).permit(:date, :log, :status)
     end
 
     def update_params
