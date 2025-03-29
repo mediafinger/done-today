a35468cd22e8413103baca6380167093c8464bf3
Andreas Finger <andy@mediafinger.com>
2025-03-29 19:50:35 +0100

    Link to all entries of each member

--- app/controllers/orgs/days_controller.rb
+++ app/controllers/orgs/days_controller.rb
@@ -1,7 +1,7 @@
 module Orgs
   class DaysController < ApplicationController
     def index
-      if params[:date]
+      if params[:date] # basically like show, but initializes a new day if none exists for the date
         @day =
           current_project.days.includes(entries: :member).find_by(date: params[:date]) ||
             current_project.days.build(org: current_org, date: params[:date])
@@ -9,6 +9,12 @@ module Orgs
         @entries = sorted_entries(@day)
 
         render :show
+      elsif params[:member_id] # show all days of the current_project for the given member
+        @members = current_project.members.order(name: :asc).pluck(:id, :name)
+        @member = current_project.members.find(params[:member_id])
+        @entries = current_project.entries.joins(:day).where(member: @member).order("day.date desc, entries.status desc", "entries.created_at asc")
+
+        render :index_of_one_member
       else
         @days = current_project.days
       end
