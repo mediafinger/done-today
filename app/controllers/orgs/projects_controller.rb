6bf2caecf4122a14f60251a45cc4acf4be89d985
Andreas Finger <andy@mediafinger.com>
2025-05-22 13:33:33 +0200

    Add project views

--- app/controllers/orgs/projects_controller.rb
+++ app/controllers/orgs/projects_controller.rb
@@ -1,9 +1,20 @@
 module Orgs
   class ProjectsController < ApplicationController
     def show
+      # TODO: only display:
+      #  * projects the member is participant of
+      #  * all projects of org, if the member role is owner
+
+      @project = current_org.projects.find(params[:id])
+      @entries_count = @project.entries.joins(:day).size
+      @days =
+        @project.days
+          .includes(entries: :member)
+          .order("days.date desc", "entries.status desc", "entries.member_id asc", "entries.created_at asc")
     end
 
     def index
+      @projects = current_org.projects.order(:name)
     end
   end
 end
